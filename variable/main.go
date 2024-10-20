99a0a6dc10889d7dc00a672f58358537441c6d12
NobSirawit <nobsirawit@gmail.com>
2024-10-21 00:37:43 +0700

    This is how you declare variable in go

---
+++ variable/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main(){
+	a := 25;
+	fmt.Println(a);
+
+	b, c, d, _, f := 1, 2, 3, 4, "happiness";
+	fmt.Println(b, c, d, f);
+
+	var g int;
+	fmt.Println(g);
+	g = 44;
+	fmt.Println(g);
+
+	var h string;
+	h = "Hello World";
+	fmt.Println(h);
+}
