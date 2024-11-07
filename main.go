f384eacbe14c68991cf74d90f2f7ab320efdc92e
NobSirawit <nobsirawit@gmail.com>
2024-11-07 16:29:09 +0700

    First time to use function in go

--- main.go
+++ main.go
@@ -3,14 +3,20 @@ package main
 import "fmt"
 
 func main(){
-	//raw string literal
-	fmt.Println(`Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sodales urna eu tellus dapibus laoreet. Suspendisse at ex sollicitudin, consequat nulla at, fringilla sapien. Aliquam ac tortor condimentum, pharetra nisi in, volutpat massa. Phasellus ac nisl magna. Vivamus non luctus nibh. Proin a commodo tortor. Praesent tristique, sapien sit amet interdum efficitur, nisl ex finibus urna, eget placerat metus turpis vel purus. Suspendisse sit amet libero sodales, commodo mauris eu, dictum enim.
-Quisque varius pretium nunc id pharetra. Etiam ultrices interdum ex quis blandit. Mauris blandit quam ut feugiat viverra. Fusce quis sapien erat. Suspendisse ultrices id diam non malesuada. Nam libero nulla, tristique nec sem eget, rutrum lobortis erat. Sed at enim felis. Nullam malesuada libero sit amet felis elementum, quis efficitur ex facilisis. Vestibulum quis rutrum ligula, in mattis ex. Maecenas et ligula sed quam volutpat aliquet quis sed lacus. Fusce feugiat luctus posuere. Phasellus ut felis enim. Pellentesque ornare facilisis lectus sed porttitor.
-Maecenas porta massa eu nunc porta faucibus. Cras luctus faucibus diam, finibus interdum turpis consectetur et. Etiam mattis ex et interdum tempor. In nibh arcu, pellentesque a ultrices quis, fermentum sed arcu. Duis sodales dapibus quam, a mattis ipsum lobortis et. Duis rhoncus molestie mauris, eget ornare ante. Phasellus lacus dui, consectetur vel dolor ac, condimentum suscipit ligula.
-Etiam vestibulum feugiat augue, dignissim rhoncus diam vulputate ac. Ut purus dui, rutrum eget hendrerit in, pellentesque nec purus. Nulla molestie purus et maximus blandit. Aliquam in consequat velit. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nunc a arcu urna. Duis in elit quis velit iaculis tristique. Curabitur ultricies diam ac turpis luctus tincidunt nec in arcu. Sed vitae orci vel nunc pellentesque finibus. Suspendisse hendrerit ipsum sed nisl volutpat, vel vestibulum elit varius. Maecenas quis posuere enim, nec ultrices dui. Integer pharetra fermentum venenatis. Sed sit amet placerat ipsum, ultricies finibus dolor.
-Etiam dictum odio arcu, non efficitur ante dapibus ut. Etiam dignissim urna et metus egestas dignissim. Integer blandit auctor lectus, vitae semper neque fringilla non. Integer tempor dapibus placerat. Praesent pulvinar porttitor neque placerat bibendum. Suspendisse potenti. Curabitur justo lectus, pellentesque ac finibus quis, semper a elit. Etiam in consectetur urna, in eleifend lacus. Maecenas eget felis consectetur, mollis neque et, rutrum risus. Nunc eleifend ex eu magna maximus, eget convallis eros mattis. Nam imperdiet elit ac laoreet porttitor. Phasellus sem arcu, laoreet in tortor a, laoreet facilisis quam. Aliquam erat volutpat. Phasellus dignissim risus libero, quis posuere elit imperdiet eu. In varius vel lacus sodales accumsan. Morbi et dapibus est.
-	`);
+	var str string = sayHello();
+	var rtn int;
+	sayHello();
+	rtn = addNumber(15, 25);
+	fmt.Println(str);
+	fmt.Println(rtn);
+}
 
-	//emoji
-	fmt.Println("😆🧐");
+func addNumber(x int, y int) int {
+	var ans int = 40;
+	ans += x + y;
+	return ans;
+}
+
+func sayHello() string{
+	return "Hello World";
 }
